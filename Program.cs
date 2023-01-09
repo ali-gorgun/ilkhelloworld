a87ef2d604234bea82d2c1de810b40e8c870d358
ali gorgun <hosteza1@gmail.com>
2023-01-09 14:58:14 +0300

    ilk hello world

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace konsol_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("isminizi girin:");
+            var name = Console.ReadLine();
+            Console.WriteLine("soyadinizi girin:");
+            var surname = Console.ReadLine();
+
+            Console.WriteLine ( " merhaba " + name + " " + surname);
+        }   
+    }
+}
